fc5fca6c14fb3cce10b1f0674ac9800645148370
Your Name <wongmdesign@gmail.com>
2022-07-05 11:28:08 -0400

    first commit

---
+++ generateTableQuads.lua
@@ -0,0 +1,34 @@
+--[[
+Given an "atlas" (a texture with multiple sprites), generate all of the
+quads for the different tiles therein, divided into tables for each set
+of tiles, since each color has 6 varieties.
+]]
+function GenerateTileQuads(atlas)
+local tiles = {}
+
+local x = 0
+local y = 0
+
+local counter = 1
+
+-- 9 rows of tiles
+for row = 1, 9 do
+		-- two sets of 6 cols, different tile varietes
+		for i = 1, 2 do
+				tiles[counter] = {}
+
+				for col = 1, 6 do
+						table.insert(tiles[counter], love.graphics.newQuad(
+								x, y, 32, 32, atlas:getDimensions()
+						))
+						x = x + 32
+				end
+
+				counter = counter + 1
+		end
+		y = y + 32
+		x = 0
+end
+
+return tiles
+end
